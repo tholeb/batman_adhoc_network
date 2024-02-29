6c45bbc87eb18e11116fa1d46d841a3d61f0f868
tholeb <43311443+tholeb@users.noreply.github.com>
2024-02-29 08:21:48 +0100

    push

--- roles/batman/files/batman-routeur.sh
+++ roles/batman/files/batman-routeur.sh
@@ -8,4 +8,8 @@ sudo ifconfig wlan0 up
 # bat0 is created via the first command
 sudo ifconfig bat0 up 
 
+iptables -t nat -A POSTROUTING -o usb0 -j MASQUERADE
+iptables -A FORWARD -i usb0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o usb0 -j ACCEPT
+
 date >> ~/.datetime
--- roles/ipforwarding/files/batman-routeur.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-batctl gw_mode server
-
-iptables -t nat -A POSTROUTING -o usb0 -j MASQUERADE
-iptables -A FORWARD -i usb0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o usb0 -j ACCEPT
